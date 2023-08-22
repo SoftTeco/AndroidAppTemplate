e91b067302b6ff8c2cc31c287e8763826a09ae79
Aliaksei Rymasheuski <a.rymasheuski@softteco.com>
2023-08-22 17:35:29 +0300

    Improving the system for extracting constants from an external file. Adding a check for the existence of a file in resources.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,132 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.util.Properties
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.ktlint)
-    alias(libs.plugins.detekt)
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
-    alias(libs.plugins.devtools.ksp) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.devtools.ksp)
 }
 
-val projectSource = file(projectDir)
-val configFile = files("$rootDir/config/detekt/detekt.yml")
-val kotlinFiles = "**/*.kt"
-val resourceFiles = "**/resources/**"
-val buildFiles = "**/build/**"
+android {
+    namespace = "com.softteco.template"
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
-apply(plugin = libs.plugins.detekt.asProvider().get().pluginId)
+    defaultConfig {
+        applicationId = "com.softteco.template"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
 
-dependencies {
-    detektPlugins(libs.plugins.detekt.formatting.get().toString())
-    detektPlugins(libs.plugins.compose.rules.detekt.get().toString())
-}
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
 
-tasks.register<io.gitlab.arturbosch.detekt.Detekt>("detektAll") {
-    description = "Custom DETEKT build for all modules"
-    parallel = true
-    ignoreFailures = false
-    autoCorrect = true
-    buildUponDefaultConfig = true
-    setSource(projectSource)
-    config.setFrom(configFile)
-    include(kotlinFiles)
-    exclude(resourceFiles, buildFiles)
-    reports {
-        html.required.set(true)
-        xml.required.set(false)
-        txt.required.set(false)
+        configurations.all {
+            resolutionStrategy {
+                force("androidx.emoji2:emoji2-views-helper:1.3.0")
+                force("androidx.emoji2:emoji2:1.3.0")
+            }
+        }
+        android.buildFeatures.buildConfig = true
+    }
+
+    buildTypes {
+
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            isDebuggable = false
+            buildConfigField("String", "BASE_URL", getPropertyByName("BASE_URL"))
+        }
+        debug {
+            isDebuggable = true
+            buildConfigField("String", "BASE_URL", getPropertyByName("BASE_URL"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+fun getPropertyByName(name: String): String {
+    project.rootProject.file("local.properties").let { file ->
+        if (file.exists()) {
+            Properties().let { properties ->
+                properties.load(file.inputStream())
+                if (properties.containsKey(name)) {
+                    return properties.getProperty(name)
+                }
+            }
+        }
+        return "\"${System.getenv(name)}\""
+    }
+}
+
+
+kapt {
+    correctErrorTypes = true
+}
+
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.lifecycle.runtime)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi.onverter)
+    implementation(libs.retrofit.coroutines.adapter)
+    implementation(libs.retrofit.converter.scalars)
+    implementation(libs.moshi)
+    ksp(libs.moshi.codegen)
+    implementation(libs.okhttp)
+    implementation(libs.okhttpLoggingInterceptor)
+
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
