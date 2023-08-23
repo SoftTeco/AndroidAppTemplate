b9cd03bb01fad1de97a624fb6c9aae931f8abbe7
savich <pcfaktor@gmail.com>
2023-08-23 16:10:10 +0300

    fix: declare the application plugin directly

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version libs.versions.applicationPlugin apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ktlint)
