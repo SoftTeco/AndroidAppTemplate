060c62ecc03e71949563336dea47c70ff25f69d2
Aliaksei Rymasheuski <a.rymasheuski@softteco.com>
2023-09-12 17:50:46 +0300

    Added object for Bluetooth screen.

--- app/src/main/kotlin/com/softteco/template/navigation/Screen.kt
+++ app/src/main/kotlin/com/softteco/template/navigation/Screen.kt
@@ -4,6 +4,8 @@ sealed class Screen(val route: String) {
     object Home : Screen("home")
     object ApiSample : Screen("api_sample")
 
+    object BLE : Screen("ble")
+
     object Profile : Screen("profile")
     object Signature : Screen("signature")
 
