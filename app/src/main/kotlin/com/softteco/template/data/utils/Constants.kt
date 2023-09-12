64f3b825bbd2dd54a9e164ff8afa4c88c91bbe74
Aliaksei Rymasheuski <a.rymasheuski@softteco.com>
2023-09-12 17:43:49 +0300

    Added necessary constants fro Bluetooth scanning and receiving.

---
+++ app/src/main/kotlin/com/softteco/template/data/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.softteco.template.data.utils
+
+const val BLUETOOTH_DEVICE_NAME = "TEMPLATE"
+const val ALL_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const val EMPTY_STRING = ""
+const val SPACE_STRING = " "
+const val MIN_SIZE_OF_RANDOM_STRING = 1
+const val MAX_SIZE_OF_RANDOM_STRING = 10
+const val BLUETOOTH_DEFAULT_TRANSMIT_DELAY = 2000L
