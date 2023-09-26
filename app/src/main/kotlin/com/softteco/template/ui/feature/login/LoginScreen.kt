7d532f1585d7605fe244fc252f339da6e9a4e268
AnastasiaShpakova <a.shpakova@softteco.com>
2023-09-26 14:11:07 +0200

    fix: fixed loading loader and app bar (#45)

--- app/src/main/kotlin/com/softteco/template/ui/feature/login/LoginScreen.kt
+++ app/src/main/kotlin/com/softteco/template/ui/feature/login/LoginScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -76,6 +78,7 @@ private fun ScreenContent(
         onDismissSnackbar = state.dismissSnackBar,
     ) {
         Column(
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(Dimens.PaddingExtraLarge),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -90,9 +93,10 @@ private fun ScreenContent(
                 verticalArrangement = Arrangement.spacedBy(Dimens.PaddingDefault),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                if (state.loading) {
-                    Text(stringResource(id = R.string.loading))
-                }
+                Text(
+                    stringResource(id = R.string.loading),
+                    modifier = Modifier.alpha(if (state.loading) 1f else 0f)
+                )
                 EmailField(state = state, Modifier.fillMaxWidth())
                 PasswordField(state = state, Modifier.fillMaxWidth())
                 Box(
