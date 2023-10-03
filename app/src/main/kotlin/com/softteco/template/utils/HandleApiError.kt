c297ec374ac27d023606258511b64ea99c5dc0e1
AnastasiaShpakova <a.shpakova@softteco.com>
2023-10-03 16:26:53 +0200

    fix: added fixes according to comments

---
+++ app/src/main/kotlin/com/softteco/template/utils/HandleApiError.kt
@@ -0,0 +1,21 @@
+package com.softteco.template.utils
+
+import com.softteco.template.R
+import com.softteco.template.data.base.error.ErrorEntity
+import com.softteco.template.data.base.error.Result
+import com.softteco.template.ui.components.SnackBarState
+import kotlinx.coroutines.flow.MutableStateFlow
+
+fun <T> handleApiError(
+    result: Result.Error<T>,
+    errorStateFlow: MutableStateFlow<SnackBarState>
+) {
+    val textId = when (result.error) {
+        is ErrorEntity.AccessDenied -> R.string.error_example
+        is ErrorEntity.Network -> R.string.error_example
+        is ErrorEntity.NotFound -> R.string.error_example
+        is ErrorEntity.ServiceUnavailable -> R.string.error_example
+        is ErrorEntity.Unknown -> R.string.error_example
+    }
+    errorStateFlow.value = SnackBarState(textId = textId, show = true)
+}
