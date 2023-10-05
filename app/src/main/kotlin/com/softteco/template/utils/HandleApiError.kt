65f1ec41e482208fe2e71491bbae70ca93907420
AnastasiaShpakova <a.shpakova@softteco.com>
2023-10-05 13:25:01 +0200

    fix: Avoid duplicating code in handling api errors in viewmodels (#30)

--- app/src/main/kotlin/com/softteco/template/utils/HandleApiError.kt
+++ app/src/main/kotlin/com/softteco/template/utils/HandleApiError.kt
@@ -11,11 +11,11 @@ fun <T> handleApiError(
     errorStateFlow: MutableStateFlow<SnackBarState>
 ) {
     val textId = when (result.error) {
-        is ErrorEntity.AccessDenied -> R.string.error_example
-        is ErrorEntity.Network -> R.string.error_example
-        is ErrorEntity.NotFound -> R.string.error_example
-        is ErrorEntity.ServiceUnavailable -> R.string.error_example
-        is ErrorEntity.Unknown -> R.string.error_example
+        is ErrorEntity.AccessDenied -> R.string.error_access_denied
+        is ErrorEntity.Network -> R.string.error_network
+        is ErrorEntity.NotFound -> R.string.error_not_found
+        is ErrorEntity.ServiceUnavailable -> R.string.error_service_unavailable
+        is ErrorEntity.Unknown -> R.string.error_unknown
     }
     errorStateFlow.value = SnackBarState(textId = textId, show = true)
 }
