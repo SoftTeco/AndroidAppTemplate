6f49cdc6ed928cdf5bc05c9dcd2a7cb0de3921a4
Anastasia Shpakova <53617426+anastasiaShpakava@users.noreply.github.com>
2023-10-06 15:59:11 +0200

    Create common component for EmailField, PasswordField (#46)
    Co-authored-by: AnastasiaShpakova <a.shpakova@softteco.com>

--- app/src/main/kotlin/com/softteco/template/ui/feature/login/LoginScreen.kt
+++ app/src/main/kotlin/com/softteco/template/ui/feature/login/LoginScreen.kt
@@ -12,36 +12,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.softteco.template.R
 import com.softteco.template.ui.components.CustomTopAppBar
+import com.softteco.template.ui.components.EmailField
+import com.softteco.template.ui.components.PasswordField
 import com.softteco.template.ui.components.TextSnackbarContainer
-import com.softteco.template.ui.feature.EmailFieldState
-import com.softteco.template.ui.feature.PasswordFieldState
 import com.softteco.template.ui.theme.AppTheme
 import com.softteco.template.ui.theme.Dimens
 
@@ -106,8 +96,18 @@ private fun ScreenContent(
                     stringResource(id = R.string.loading),
                     modifier = Modifier.alpha(if (state.loading) 1f else 0f)
                 )
-                EmailField(state = state, Modifier.fillMaxWidth())
-                PasswordField(state = state, Modifier.fillMaxWidth())
+                EmailField(
+                    emailValue = state.emailValue,
+                    onEmailChanged = state.onEmailChanged,
+                    fieldStateEmail = state.fieldStateEmail,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                PasswordField(
+                    passwordValue = state.passwordValue,
+                    onPasswordChanged = state.onPasswordChanged,
+                    fieldStatePassword = state.fieldStatePassword,
+                    modifier = Modifier.fillMaxWidth()
+                )
                 Box(
                     modifier = Modifier.padding(
                         Dimens.PaddingNormal,
@@ -155,73 +155,6 @@ private fun ScreenContent(
                 )
             }
         }
-    }
-}
-
-@Composable
-private fun EmailField(
-    state: LoginViewModel.State,
-    modifier: Modifier = Modifier,
-) {
-    Column {
-        OutlinedTextField(
-            value = state.emailValue,
-            onValueChange = {
-                state.onEmailChanged(it)
-            },
-            modifier = modifier,
-            label = {
-                Text(text = stringResource(id = R.string.email))
-            },
-            isError = state.fieldStateEmail is EmailFieldState.Empty || state.fieldStateEmail is EmailFieldState.Error
-        )
-        val errorText =
-            when (state.fieldStateEmail) {
-                is EmailFieldState.Empty -> stringResource(R.string.required)
-                is EmailFieldState.Error -> stringResource(R.string.email_not_valid)
-                else -> ""
-            }
-        Text(errorText, color = MaterialTheme.colorScheme.error)
-    }
-}
-
-@Composable
-private fun PasswordField(
-    state: LoginViewModel.State,
-    modifier: Modifier = Modifier,
-) {
-    Column {
-        var passwordVisibility by remember { mutableStateOf(true) }
-        OutlinedTextField(
-            value = state.passwordValue,
-            onValueChange = {
-                state.onPasswordChanged(it)
-            },
-            modifier = modifier,
-            label = {
-                Text(text = stringResource(id = R.string.password))
-            },
-            isError = state.fieldStatePassword is PasswordFieldState.Empty,
-            trailingIcon = {
-                IconButton(onClick = {
-                    passwordVisibility = !passwordVisibility
-                }) {
-                    Icon(
-                        imageVector = if (passwordVisibility) Icons.Filled.VisibilityOff else Icons.Filled.Visibility,
-                        contentDescription = stringResource(id = R.string.visibility),
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-                }
-            },
-            visualTransformation = if (passwordVisibility) PasswordVisualTransformation() else VisualTransformation.None
-        )
-        val errorText =
-            when (state.fieldStatePassword) {
-                PasswordFieldState.Empty -> stringResource(R.string.required)
-                is PasswordFieldState.Error -> ""
-                else -> ""
-            }
-        Text(errorText, color = MaterialTheme.colorScheme.error)
     }
 }
 
