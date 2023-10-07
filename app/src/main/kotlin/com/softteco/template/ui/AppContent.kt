68e8e4fcf15573e9c39dc17a6729a036bb309c1e
Bogdan Stolyarov <b.stolyarov.v@gmail.com>
2023-10-07 23:09:46 +0300

    fix: fix content jumping after navigating from the Home screen to the Login screen (#63)

--- app/src/main/kotlin/com/softteco/template/ui/AppContent.kt
+++ app/src/main/kotlin/com/softteco/template/ui/AppContent.kt
@@ -1,6 +1,7 @@
 package com.softteco.template.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,6 +16,7 @@ fun AppContent(modifier: Modifier = Modifier) {
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
         bottomBar = {
             AppBottomBar(navController = navController)
         }
