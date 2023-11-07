846d6bd0475e683ec41c24bb6217fa4364753f81
Evgeniy Filippov <6479556@gmail.com>
2023-11-07 18:25:59 +0300

    refactor: fix detekt

--- app/src/main/kotlin/com/softteco/template/App.kt
+++ app/src/main/kotlin/com/softteco/template/App.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class MyApplication : Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
