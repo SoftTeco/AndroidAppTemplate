9c6c2aa7e63dfec3f17b6c098cde557598ab6ccc
AnastasiaShpakova <a.shpakova@softteco.com>
2023-11-16 19:46:08 +0100

    fix: refactoring (#71)

--- app/src/test/kotlin/com/softteco/template/ui/feature/signUp/SignUpViewModelTest.kt
+++ app/src/test/kotlin/com/softteco/template/ui/feature/signUp/SignUpViewModelTest.kt
@@ -5,6 +5,8 @@ import com.softteco.template.BaseTest
 import com.softteco.template.data.base.error.Result
 import com.softteco.template.data.profile.ProfileRepository
 import com.softteco.template.data.profile.dto.CreateUserDto
+import com.softteco.template.ui.feature.EmailFieldState
+import com.softteco.template.ui.feature.PasswordFieldState
 import com.softteco.template.utils.MainDispatcherExtension
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
@@ -56,7 +58,7 @@ class SignUpViewModelTest : BaseTest() {
         }
 
     @Test
-    fun `when sign-up button isn't enabled and invalid email then error state is emitted`() =
+    fun `when invalid email then email field error is shown and sign-up button isn't enabled`() =
         runTest {
             viewModel = SignUpViewModel(repository)
             viewModel.state.test {
@@ -67,12 +69,13 @@ class SignUpViewModelTest : BaseTest() {
 
                 expectMostRecentItem().run {
                     isSignupBtnEnabled shouldBe false
+                    fieldStateEmail shouldBe EmailFieldState.Error
                 }
             }
         }
 
     @Test
-    fun `when sign-up button isn't enabled and invalid password then error state is emitted`() =
+    fun `when password hasn't capital letter then password field error is shown and sign-up button isn't enabled`() =
         runTest {
             viewModel = SignUpViewModel(repository)
             viewModel.state.test {
@@ -82,13 +85,15 @@ class SignUpViewModelTest : BaseTest() {
                 delay(1.seconds)
 
                 expectMostRecentItem().run {
+                    fieldStatePassword.shouldBeTypeOf<PasswordFieldState.Error>()
+                    (fieldStatePassword as PasswordFieldState.Error).isUppercase shouldBe false
                     isSignupBtnEnabled shouldBe false
                 }
             }
         }
 
     @Test
-    fun `when sign-up button isn't enabled and field password has not enough symbols then error state is emitted`() =
+    fun `when password hasn't enough symbols then password field error is shown and sign-up button isn't enabled`() =
         runTest {
             viewModel = SignUpViewModel(repository)
             viewModel.state.test {
@@ -98,13 +103,15 @@ class SignUpViewModelTest : BaseTest() {
                 delay(1.seconds)
 
                 expectMostRecentItem().run {
+                    fieldStatePassword.shouldBeTypeOf<PasswordFieldState.Error>()
+                    (fieldStatePassword as PasswordFieldState.Error).isRightLength shouldBe false
                     isSignupBtnEnabled shouldBe false
                 }
             }
         }
 
     @Test
-    fun `when sign-up button isn't enabled and empty username then error state is emitted`() =
+    fun `when empty username then sign-up button isn't enabled`() =
         runTest {
             viewModel = SignUpViewModel(repository)
             viewModel.state.test {
@@ -119,11 +126,13 @@ class SignUpViewModelTest : BaseTest() {
         }
 
     @Test
-    fun `when sign-up button isn't enabled and empty username, email, and password then error state is emitted`() =
+    fun `when empty username, email, password then password, email fields error are shown and button isn't enabled`() =
         runTest {
             viewModel = SignUpViewModel(repository)
             viewModel.state.test {
                 awaitItem().run {
+                    fieldStatePassword shouldBe PasswordFieldState.Empty
+                    fieldStateEmail shouldBe EmailFieldState.Empty
                     isSignupBtnEnabled shouldBe false
                 }
             }
