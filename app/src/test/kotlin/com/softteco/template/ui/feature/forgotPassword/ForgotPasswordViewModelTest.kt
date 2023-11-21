19d2dd6709fc1d63d918c4a7d0e584e4127c2cd4
Anastasia Shpakova <53617426+anastasiaShpakava@users.noreply.github.com>
2023-11-21 17:14:34 +0300

    feat: add unit tests for the Restore password screen  (#73)
    Co-authored-by: AnastasiaShpakova <a.shpakova@softteco.com>

---
+++ app/src/test/kotlin/com/softteco/template/ui/feature/forgotPassword/ForgotPasswordViewModelTest.kt
@@ -0,0 +1,107 @@
+package com.softteco.template.ui.feature.forgotPassword
+
+import app.cash.turbine.test
+import com.softteco.template.BaseTest
+import com.softteco.template.data.base.error.Result
+import com.softteco.template.data.profile.ProfileRepository
+import com.softteco.template.data.profile.dto.ResetPasswordDto
+import com.softteco.template.ui.feature.EmailFieldState
+import com.softteco.template.utils.MainDispatcherExtension
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeTypeOf
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.time.Duration.Companion.seconds
+
+@ExtendWith(MainDispatcherExtension::class)
+class ForgotPasswordViewModelTest : BaseTest() {
+
+    @RelaxedMockK
+    private lateinit var repository: ProfileRepository
+    private lateinit var viewModel: ForgotPasswordViewModel
+    private val email = "test@email.com"
+
+    @Test
+    fun `when valid email and reset password button is enabled then success state is emitted`() =
+        runTest {
+            coEvery { repository.resetPassword(ResetPasswordDto(email)) } returns Result.Success(
+                Unit
+            )
+            viewModel = ForgotPasswordViewModel(repository)
+
+            viewModel.state.test {
+                awaitItem().onEmailChanged(email)
+                delay(1.seconds)
+
+                expectMostRecentItem().run {
+                    isResetBtnEnabled shouldBe true
+                    onRestorePasswordClicked()
+                }
+
+                awaitItem().run {
+                    forgotPasswordState.shouldBeTypeOf<ForgotPasswordViewModel.ForgotPasswordState.Success>()
+                    snackBar.show shouldBe true
+                }
+            }
+
+            coVerify(exactly = 1) { repository.resetPassword(ResetPasswordDto(email)) }
+        }
+
+    @Test
+    fun `when invalid email then reset password button isn't enabled and email field error is shown`() =
+        runTest {
+            viewModel = ForgotPasswordViewModel(repository)
+            viewModel.state.test {
+                awaitItem().onEmailChanged("invalid@email")
+                delay(1.seconds)
+
+                expectMostRecentItem().run {
+                    isResetBtnEnabled shouldBe false
+                    fieldStateEmail shouldBe EmailFieldState.Error
+                }
+            }
+        }
+
+    @Test
+    fun `when empty email then reset password button isn't enabled and email field error is shown`() =
+        runTest {
+            viewModel = ForgotPasswordViewModel(repository)
+            viewModel.state.test {
+                awaitItem().run {
+                    isResetBtnEnabled shouldBe false
+                    fieldStateEmail shouldBe EmailFieldState.Empty
+                }
+            }
+        }
+
+    @Test
+    fun `when reset password button clicked and request in progress then loading is shown`() =
+        runTest {
+            coEvery { repository.resetPassword(ResetPasswordDto(email)) } coAnswers {
+                delay(1.seconds)
+                Result.Success(Unit)
+            }
+            viewModel = ForgotPasswordViewModel(repository)
+
+            viewModel.state.test {
+                awaitItem().onEmailChanged(email)
+                delay(1.seconds)
+
+                expectMostRecentItem().run {
+                    isResetBtnEnabled shouldBe true
+                    onRestorePasswordClicked()
+                }
+
+                awaitItem().run {
+                    forgotPasswordState.shouldBeTypeOf<ForgotPasswordViewModel.ForgotPasswordState.Loading>()
+                }
+            }
+
+            coVerify(exactly = 1) { repository.resetPassword(ResetPasswordDto(email)) }
+        }
+}
