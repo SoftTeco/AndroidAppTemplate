eebb965175b03098d968003d2d3342264d9dfb13
savich <pcfaktor@gmail.com>
2024-04-12 14:19:16 +0300

    fix: temporary handle token expiration by logging out (#168)

--- app/src/main/kotlin/com/softteco/template/MainViewModel.kt
+++ app/src/main/kotlin/com/softteco/template/MainViewModel.kt
@@ -4,27 +4,36 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.softteco.template.data.profile.ProfileRepository
 import com.softteco.template.ui.feature.settings.PreferencesKeys
 import com.softteco.template.ui.theme.ThemeMode
 import com.softteco.template.utils.AppDispatchers
-import com.softteco.template.utils.SessionManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     dataStore: DataStore<Preferences>,
-    sessionManager: SessionManager,
-    val appDispatchers: AppDispatchers,
+    appDispatchers: AppDispatchers,
+    private val profileRepository: ProfileRepository,
 ) : ViewModel() {
 
     val theme = dataStore.data
         .map { it[PreferencesKeys.THEME_MODE] ?: ThemeMode.SystemDefault.value }
         .stateIn(viewModelScope, SharingStarted.Lazily, ThemeMode.SystemDefault.value)
 
-    val isUserLoggedIn = sessionManager.isUserLoggedIn
-        .stateIn(viewModelScope, SharingStarted.Lazily, null)
+    val isUserLoggedIn = MutableStateFlow<Boolean?>(null)
+
+    init {
+        viewModelScope.launch(appDispatchers.io) {
+            profileRepository.getUser().let { result ->
+                isUserLoggedIn.value = result.isSuccess
+            }
+        }
+    }
 }
--- app/src/main/kotlin/com/softteco/template/data/base/error/Result.kt
+++ app/src/main/kotlin/com/softteco/template/data/base/error/Result.kt
@@ -5,4 +5,8 @@ sealed class Result<T> {
     data class Success<T>(val data: T) : Result<T>()
 
     data class Error<T>(val error: AppError) : Result<T>()
+
+    val isSuccess: Boolean get() = this !is Error
+
+    val isFailure: Boolean get() = this is Error
 }
