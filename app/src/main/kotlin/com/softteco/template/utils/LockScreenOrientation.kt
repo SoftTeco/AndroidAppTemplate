8cf77d9e14a07232c07abf2c1aa9d617e6488ecd
savich <pcfaktor@gmail.com>
2024-06-10 15:34:21 +0300

    fix: improve getting the original orientation (#173)

--- app/src/main/kotlin/com/softteco/template/utils/LockScreenOrientation.kt
+++ app/src/main/kotlin/com/softteco/template/utils/LockScreenOrientation.kt
@@ -1,5 +1,6 @@
 package com.softteco.template.utils
 
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.platform.LocalContext
@@ -7,7 +8,9 @@ import androidx.compose.ui.platform.LocalContext
 @Composable
 fun LockScreenOrientation(orientation: Int) {
     val activity = LocalContext.current.getActivity() ?: return
-    val originalOrientation = activity.resources.configuration.orientation
+    val originalOrientation = activity.resources.configuration.orientation.run {
+        if (this == ORIENTATION_LANDSCAPE) this else activity.requestedOrientation
+    }
 
     DisposableEffect(orientation) {
         activity.requestedOrientation = orientation
